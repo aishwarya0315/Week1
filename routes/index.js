aee8ebb5d6518735f4e54a89325f735f5bdc915b
maris <aishwarya.mariselvam@gmail.com>
2023-07-29 08:10:30 +1000

    updated code

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
-const nameList = [
-  {id: '1', name: 'John', email:'John@gmail.com'},
-  {id: '2', name: 'Aish', email:'Aish@gmail.com'},
-];
+// index.js
+
+const glossaryData = require('./devops_glossary.json');
+// Function to display glossary term by ID
+// Display all glossary terms
+console.log("DevOps Glossary:");
+console.log("-----------------");
+glossaryData.forEach(term => {
+  console.log(`Id: ${term.Id}`);
+  console.log(`Term: ${term.Term}`);
+  console.log(`Description: ${term.Description}`);
+  console.log(`Reference: ${term.Reference}`);
+  console.log("-----------------");
+});
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'SIT722', sub: 'ABC' , tble: nameList});
+  res.render('index', { title: 'SIT722', sub: 'ABC' , tble: glossaryData});
 });
 
 module.exports = router;
+
+
+
+
+
+
+
+
